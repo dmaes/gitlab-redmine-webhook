217641bb6da79991937bdf9d8cdff2d7aecf9402
Dieter Maes <dmaes@inuits.eu>
2025-11-20 12:54:48 +0100

    add release workflow

---
+++ .github/gen-release-notes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Releasing ${CI_COMMIT_TAG}"
+
+CHANGELOG_FILE=CHANGELOG.md
+
+VERSION="$(echo "$GITHUB_REF" | sed -E 's/^v//')"
+
+grep -E "^## \[v?${VERSION}\]" "$CHANGELOG_FILE" \
+  | sed -E "s/^## \[v?${VERSION}\] (.+)$/# v${VERSION} \1/g" \
+  > release_notes.md
+
+echo "## Changelog" >> release_notes.md
+sed -rn "/^## \[v?${VERSION}\]/,/^## \[/p" "$CHANGELOG_FILE" \
+  | grep -vE '^## \[' >> release_notes.md;
+
+cat release_notes.md
