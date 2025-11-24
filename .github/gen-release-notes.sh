b788efb434ac6e3d7ee09e24996fe8d31407f90e
Dieter Maes <dmaes@inuits.eu>
2025-11-24 14:08:19 +0100

    mrege build-push and release

--- .github/gen-release-notes.sh
+++ .github/gen-release-notes.sh
@@ -6,10 +6,8 @@ set -eo pipefail
 
 echo "Releasing ${CI_COMMIT_TAG}"
 
-CHANGELOG_FILE=CHANGELOG.md
+CHANGELOG_FILE="${CHANGELOG_FILE:-CHANGELOG.md}"
 NOTES=.github/release_notes.md
-
-VERSION="$(echo "$GITHUB_REF" | sed -E 's/^refs\/tags\/v//')"
 
 grep -E "^## \[v?${VERSION}\]" "$CHANGELOG_FILE" \
   | sed -E "s/^## \[v?${VERSION}\] (.+)$/# v${VERSION} \1/g" \
